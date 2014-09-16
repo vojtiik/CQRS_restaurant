c5489bfd12dae6e273d53f5a25847403a2e2f658
vojtiik <vojtechmares@googlemail.com>
2014-09-16 15:25:46 +0100

    added basic events

---
+++ CQRS_restaurant/BaseEvent.cs
@@ -0,0 +1,26 @@
+﻿namespace CQRS_restaurant
+{
+    public class BaseEvent : IMessage
+    {
+        public string EventId { get; set; }
+    }
+
+    public class OrderPlaced : BaseEvent
+    {
+        public Order Order { get; set; }
+    }
+
+    public class OrderPriced : BaseEvent
+    {
+        public Order Order { get; set; }
+    }
+
+    public class OrderCooked : BaseEvent
+    {
+        public Order Order { get; set; }
+    }
+
+    public interface IMessage
+    {
+    }
+}
